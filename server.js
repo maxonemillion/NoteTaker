123da24bbc99afa5f869185966b28d1de373b082
maxonemillion <max.daniel@utah.edu>
2020-10-08 23:09:08 -0600

    updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const db = require('./db/db.json');
+const { v4: uuidv4 } = require('uuid');
 
 var app = express();
 var PORT = process.env.PORT || 6969;
@@ -10,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./public'));
 
-// GETS
+// GET
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
@@ -24,20 +25,25 @@ app.get("/api/notes", function(req, res) {
     return res.json(db);
 });
 
-// POSTS
+// POST
 
 app.post('/api/notes', function (req, res) {
     const note = req.body;
+    note.id = uuidv4();
     console.log('post working', note);
-    res.end();
-    
+    db.push(note);
+    fs.writeFileSync(path.join(__dirname, 'db', 'db.json'), JSON.stringify(db));
+    res.json(note);
 });
 
-// DELETES
+// DELETE
 
 app.delete('/api/notes/:id', function (req, res) {
-    console.log('delete working');
-    res.end();
+    const exnote = req.params.id;
+    console.log('delete working', exnote);
+    db = db.filter((note) => note.id !== req.params.id);
+    fs.writeFileSync(path.join(__dirname, 'db', 'db.json'), JSON.stringify(db));
+    res.json({ok:true});
 });
 
 app.listen(PORT, function () {
